435cb35bc61ef2915db712b513ce747e0d249211
LWitfoth <lisa.a.witfoth@outlook.com>
2022-05-03 14:59:50 -0400

    Class Library Added

---
+++ source/repos/JSPrac/js/main.js
@@ -0,0 +1,12 @@
+function myTest(){
+    alert("Hello");
+    console.log("this is better because it doesn't pop out!");
+}
+function myFunc() {
+    document.getElementById("demo").style.fontSize = "200px";
+}
+
+function 
+
+let str = "Apple, Banana, Kiwi";
+document.getElementById("demoSubstring").innerHTML = str.substring(7,13);
